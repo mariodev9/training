ce3c0709b4da10ee65f254f78072d5964f1fb70d
LucianoMariotti <luciano.mariotti99@gmail.com>
2022-04-29 20:45:45 -0300

    add button, change header styles

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,83 +1,99 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../assets/css/Nav.css";
+import { motion } from "framer-motion";
 
 export default function Nav() {
   return (
-    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
-      <Link className="navbar-brand" to={"/"}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="42"
-          height="21"
-          viewBox="0 0 42 21"
-          fill="none"
-        >
-          <path
-            d="M13.44 21H25.2C34.5 21 34.02 0 23.52 0H13.44V8.25L17.64 3H23.52C28.56 3 31.08 15.8431 24.36 17.25H17.64V7.5L13.44 12.75V21Z"
-            fill="black"
-          />
-          <path
-            d="M35.28 9V0.75L39.48 4.5V6.75L36.96 4.5V9H35.28Z"
-            fill="black"
-          />
-          <path d="M40.32 0.75V7.5L42 9V0.75H40.32Z" fill="black" />
-          <path
-            d="M5.03997 5.25L8.39997 0.75L10.08 9H8.39997L7.55997 4.5L5.03997 8.25L3.36 5.78909L3.77997 3.375L5.03997 5.25Z"
-            fill="black"
-          />
-          <path
-            d="M1.68 0.75L0 9H1.68L2.47999 4.5L2.87998 2.25L1.68 0.75Z"
-            fill="black"
-          />
-        </svg>
-        <span className="logo-text">Entrenamiento</span>
-      </Link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+    <div className="container">
+      <motion.nav
+        animate={{
+          y: [-100, 0],
+          opacity: [0, 1],
+          transition: {
+            ease: [0.6, 0.01, -0.05, 0.95],
+            duration: 1,
+          },
+        }}
+        class="navbar navbar-expand-lg navbar-light bg-light "
       >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse " id="navbarNav">
-        <ul class="navbar-nav">
-          <Link to={"/About"} class="nav-item">
-            Quienes somos
-          </Link>
-          <Link to={"/Horarios"} class="nav-item">
-            Horarios
-          </Link>
-          <Link to={"/Contact"} class="nav-item">
-            Contact
-          </Link>
-          <Link to={"/instagram"} class="nav-item">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="black"
-            >
-              <circle cx="12" cy="12" r="5.75" stroke-width="2.5" />
-              <circle cx="18.5" cy="5.5" r="1.5" fill="black" />
-              <rect
-                x="1.25"
-                y="1.25"
-                width="21.5"
-                height="21.5"
-                rx="3.75"
-                stroke-width="2.5"
-              />
-            </svg>
-          </Link>
-        </ul>
-      </div>
-    </nav>
+        <Link className="navbar-brand" to={"/"}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="42"
+            height="21"
+            viewBox="0 0 42 21"
+            fill="none"
+          >
+            <path
+              d="M13.44 21H25.2C34.5 21 34.02 0 23.52 0H13.44V8.25L17.64 3H23.52C28.56 3 31.08 15.8431 24.36 17.25H17.64V7.5L13.44 12.75V21Z"
+              fill="black"
+            />
+            <path
+              d="M35.28 9V0.75L39.48 4.5V6.75L36.96 4.5V9H35.28Z"
+              fill="black"
+            />
+            <path d="M40.32 0.75V7.5L42 9V0.75H40.32Z" fill="black" />
+            <path
+              d="M5.03997 5.25L8.39997 0.75L10.08 9H8.39997L7.55997 4.5L5.03997 8.25L3.36 5.78909L3.77997 3.375L5.03997 5.25Z"
+              fill="black"
+            />
+            <path
+              d="M1.68 0.75L0 9H1.68L2.47999 4.5L2.87998 2.25L1.68 0.75Z"
+              fill="black"
+            />
+          </svg>
+          <span className="logo-text">Entrenamiento</span>
+        </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarNav">
+          <ul class="navbar-nav">
+            <Link to={"/About"} class="nav-item">
+              Quienes somos
+            </Link>
+            <Link to={"/"} class="nav-item">
+              Entrenamiento
+            </Link>
+            <Link to={"/Horarios"} class="nav-item">
+              Horarios
+            </Link>
+            <Link to={"/Contact"} class="nav-item">
+              Contact
+            </Link>
+            <Link to={"/instagram"} class="nav-item">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="black"
+              >
+                <circle cx="12" cy="12" r="5.75" stroke-width="2.5" />
+                <circle cx="18.5" cy="5.5" r="1.5" fill="black" />
+                <rect
+                  x="1.25"
+                  y="1.25"
+                  width="21.5"
+                  height="21.5"
+                  rx="3.75"
+                  stroke-width="2.5"
+                />
+              </svg>
+            </Link>
+          </ul>
+        </div>
+      </motion.nav>
+    </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,45 +1,69 @@
 import React from "react";
 import "../assets/css/Home.css";
 import { motion } from "framer-motion";
+import atlhete from "../assets/static/Atlhete.png";
+import Training from "../components/Training";
+import Button from "../components/Button/Button";
 
 export default function Home() {
-  const banner = {
-    animate: {
-      transition: {
-        delayChildren: 0.4,
-        staggerChildren: 0.1,
-      },
-    },
-  };
-
   const letterAnimation = {
     initial: {
       y: 400,
+      opacity: 0,
     },
     animate: {
       y: 0,
+      opacity: 1,
       transition: {
         ease: [0.6, 0.01, -0.05, 0.95],
-        duration: 1,
+        duration: 1.5,
       },
     },
   };
 
   return (
-    <motion.div
-      className="container"
-      initial={{ opacity: 0, transition: 1 }}
-      animate={{ opacity: 1 }}
-      exit={{ x: window.innerWidth, transition: 0.3 }}
-    >
-      <div className="row">
-        <div className="col-md-6 banner">
-          <h1>Entrená</h1>
-          <h1>Mejorá</h1>
-          <h1>Competí</h1>
-        </div>
-        <div className="col-md-6 home2"></div>
-      </div>
-    </motion.div>
+    <>
+      <motion.div
+        className="container"
+        initial={{ opacity: 0, transition: 1 }}
+        animate={{ opacity: 1 }}
+        exit={{ x: window.innerWidth, transition: 0.3 }}
+      >
+        {/* COMPONENT HEADER */}
+        <header>
+          <div className="row">
+            <motion.div
+              className="col-md-8 banner"
+              variants={letterAnimation}
+              initial="initial"
+              animate="animate"
+            >
+              <div className="gradient">
+                <h1>Forma parte de MDN</h1>
+                <h1>Sé un atleta</h1>
+              </div>
+              <Button />
+            </motion.div>
+            <div className="col-md-4 athlete-container">
+              <motion.img
+                className="athlete-image"
+                src={atlhete}
+                alt=""
+                // FALTA INITIAL
+                animate={{
+                  x: [300, 0],
+                  opacity: [0, 1],
+                  transition: {
+                    ease: [0.6, 0.01, -0.05, 0.95],
+                    duration: 1,
+                  },
+                }}
+              />
+            </div>
+          </div>
+        </header>
+        <Training />
+      </motion.div>
+    </>
   );
 }
